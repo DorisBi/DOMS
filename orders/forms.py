dec421c657c577ba73737b29ffa80b1907e0c6b9
danyu <danyu.bi@intel.com>
2019-08-15 14:46:27 +0800

    modify database variables to match real requirements
    Signed-off-by: danyu <danyu.bi@intel.com>

--- orders/forms.py
+++ orders/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django import forms
 from .models import Order
 
+
 class OrderForm(ModelForm):
     OPTIONS = (
         ('',''),
@@ -22,4 +23,6 @@ class OrderForm(ModelForm):
 
     class Meta:
         model = Order
-        fields = ['name','phone','address','delivery_date','product_id','payment_option','amount','order_status']
+        fields = ['customer_name', 'customer_phone', 'customer_address', 'creation_date', 'product_id',
+                  'payment_option', 'number_of_packets', 'weight', 'inventory_warehouse', 'transportation_vender',
+                  'delivery_order_id', 'order_status']
--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
 class Order (models.Model):
-    name = models.CharField(max_length=200);
-    phone = models.CharField(max_length=20);
-    address = models.TextField();
-    delivery_date = models.DateField(blank=True);
-    product_id = models.TextField();
-    payment_option = models.CharField(max_length=50);
-    amount = models.IntegerField();
-    order_status = models.CharField(max_length=50);
+    customer_name = models.CharField(max_length=200)
+    customer_phone = models.CharField(max_length=20)
+    customer_address = models.TextField()
+    creation_date = models.DateField(blank=True)
+    product_id = models.TextField()
+    payment_option = models.CharField(max_length=50)
+    number_of_packets = models.IntegerField()
+    weight = models.IntegerField()
+    inventory_warehouse = models.CharField(max_length=200)
+    transportation_vender = models.CharField(max_length=200)
+    delivery_order_id = models.CharField(max_length=50)
+    order_status = models.CharField(max_length=50)
